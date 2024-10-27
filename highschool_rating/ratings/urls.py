404f03dd0346f2ddd552a809f781b1db67497a5c
codemonarch <oluwasemilogoadeogun@gmail.com>
2024-10-27 13:59:52 +0100

    configured the urls.py

---
+++ highschool_rating/ratings/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import HighSchoolViewSet, ReviewViewSet, register, user_login
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'highschools', HighSchoolViewSet)
+router.register(r'reviews', ReviewViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/', register),
+    path('login/', user_login),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
